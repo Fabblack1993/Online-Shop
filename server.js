9cf78c47469e68c36fb2979ca6fd44138cbdbf50
Ndouga Fabienne <ndougafabienne77@.com>
2025-04-01 03:23:17 +0100

    login modification

--- server.js
+++ server.js
@@ -35,9 +35,13 @@ app.get('/cancel', (req, res) => {
 
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
-    // Exemple basique de validation (remplace avec une base de données sécurisée)
-    if (username === 'admin' && password === 'C@brelle2017') {
-        res.redirect('/dashboard.html'); // Redirige vers le tableau de bord
+
+    // Exemple de validation : Remplace ces valeurs avec des identifiants stockés dans une base de données
+    const validUsername = "admin";
+    const validPassword = "password123";
+
+    if (username === validUsername && password === validPassword) {
+        res.redirect('/dashboard.html'); // Redirige vers le tableau de bord en cas de succès
     } else {
         res.status(401).send("Nom d'utilisateur ou mot de passe incorrect");
     }
